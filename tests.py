e6ed405b7c94f2865932d040485a3fa493840681
Christopher H. Casebeer <christopher@chc.name>
2012-07-30 05:09:04 -0400

    Refactor for easy new statuses; add statuses.
    Refactor webpy_helpers to permit much easier adding of new customized
    HTTPError exception classes.
    
    Move basic HTML custom error functionality into a common base class,
    letting us specify only the status code and message for each new
    error class.
    
    Move basic JSON custom error functionality into an automatically
    generated constructor applied via a custom metaclass, allowing us to
    create a standardized JSON response corresponding to one of our
    HTML HTTPError classes by simply subclassing and setting the correct
    __metaclass__ property.
    
    Refactor the tests module to handle a greater number of errors to test:
    Rather than manually create three test controllers for each HTTPError
    (one for the HTML class, two for the corresponding JSON class, since
    we want to test both dict and string error messages via JSON), we now
    have a helper function that auto generates these three controller
    classes, using `type` to programmatically create classes, given the
    base class name being tested, the expected and specified error response
    bodies, and the expected status code.
    
    Use this controller creation helper to generate the three required
    controllers for each of the (base) error class names in a dictionary
    of class names and expected status codes.
    
    Run the list of generated classes (concatenated with the list of manually
    written TestController subclasses, if any) as before via a Nose test
    generator.
    
    The only change to test execution is that we can now expect JSON
    response bodies; if so, we'll attempt to JSON decode the recieved
    error response body before comparing the resulting Python dict to the
    "expected" dict specified in the test controller class.

--- tests.py
+++ tests.py
@@ -1,63 +1,180 @@
+'''
+Tests for webpy_helpers
+
+- Automatically generated web.py test controllers for all HTTPError
+  exceptions.
+
+Run tests via Nose:
+
+    $ nosetests
+    ....................................
+    ----------------------------------------------------------------------
+    Ran 36 tests in 0.372s
+    
+    OK
+
+or setup.py:
+
+    $ python setup.py test
+
+TODO: More in depth manual testing of basic exception funcationality
+      for one HTTPError subclass, since most behavior is shared amongst 
+	  all the error classes via subclassing and metaclasses. 
+'''
 
 from paste.fixture import TestApp
 from nose.tools import *
 
 import itertools
+import json
 
 import web
 import webpy_helpers
 
+# Dictionary to automatically generate test controllers
+#
+# For each key, the corresponding class with the same name, plus the
+# class with that name preceded by "Json" in the webpy_helpers module
+# will be exercised, basic expected functionality checked, and the 
+# HTTP status code compared to the key's value.
+tests = {
+	"BadRequest": 400,
+	"Unauthorized": 401,
+	"Forbidden": 403,
+	"NotFound": 404,
+	"MethodNotAllowed": 405,
+	"NoMethod": 405,
+	"Conflict": 409,
+	"Gone": 410,
+	"UnsupportedMediaType": 415,
+	"RequestedRangeNotSatisfiable": 416,
+	"InternalServerError": 500,
+	"InternalError": 500,
+}
+
 class TestController(object):
+	'''Base class for all web.py test controllers.'''
 	@classmethod
 	def path(cls):
 		return "/%s" % cls.__name__
 
-#### Web.py controller classes testing each exception
-class BadRequest(TestController):
-	status = 400
-	body = "foobar"
-	def GET(self):
-		raise webpy_helpers.BadRequest("foobar")
-class JsonBadRequest(BadRequest):
-	body = """{"message": "foobar"}"""
-	def GET(self):
-		raise webpy_helpers.JsonBadRequest({"message":"foobar"})
-class JsonBadRequest_string(JsonBadRequest):
-	def GET(self):
-		raise webpy_helpers.JsonBadRequest("foobar")
+def make_class(status_code, class_name, test_name, message_argument, expected_body):
+	'''Generate a subclass of TestController with an automatically generated GET method.'''
+	return type("%s%sTest" % (class_name, test_name),
+		(TestController,),
+		{
+			'status': status_code,
+			'body': expected_body,
+			'GET': make_GET_method(class_name, message_argument)
+		}
+	)
 
-class NotFound(TestController):
-	status = 404
-	body = "foobar"
-	def GET(self):
-		raise webpy_helpers.NotFound("foobar")
-class JsonNotFound(NotFound):
-	body = """{"message": "foobar"}"""
-	def GET(self):
-		raise webpy_helpers.JsonNotFound({"message":"foobar"})
-class JsonNotFound_string(JsonNotFound):
-	def GET(self):
-		raise webpy_helpers.JsonNotFound("foobar")
+def make_GET_method(class_name, message_argument):
+	if message_argument:
+		def GET(self):
+			raise getattr(webpy_helpers, class_name)(message_argument)
+	else:
+		def GET(self):
+			raise getattr(webpy_helpers, class_name)()
+	return GET
 
-# enumerate all test classes
-classes = [cls for name,cls in locals().items() if type(cls) == type and issubclass(cls, TestController) and cls != TestController ]
+def make_webpy_controllers(status_code, class_name):
+	'''
+	Make test controllers for a particular HTTP Error class
 
-urls = tuple(itertools.chain(*[(cls.path(), cls) for cls in classes]))
+	For the provided status code and class name, generate three test controllers:
 
-print urls
+	1. For the named class, testing that HTML messages are passed through.
+	2. For the class name prefixed with "Json," testing that JSON dicts are
+	   passed through as response bodies.
+	3. For the class name prefied with "Json," testing that string messages are
+	   properly wrapped in JSON dicts and passed through as response bodies.
+	'''
 
-app = web.application(urls, globals())
+	html_test_class = make_class(
+								status_code,
+								class_name,
+								"Html",
+								"<p>This is the HTML test message.</p>",
+								"<p>This is the HTML test message.</p>"
+								)
+	json_test_class = make_class(
+								status_code,
+								"Json%s" % (class_name,),
+								"",
+								{"message" : "This is the JSON dict-based test message."},
+								{"message" : "This is the JSON dict-based test message."}
+								)
+	json_string_test_class = make_class(
+								status_code,
+								"Json%s" % (class_name,),
+								"String",
+								"This is the JSON str-based test message.",
+								{"message": "This is the JSON str-based test message."}
+								)
+	return [html_test_class, json_test_class, json_string_test_class]
+
+#### Gather the web.py controller classes testing each exception
+
+# generate automatic test controllers
+controllers = list(itertools.chain.from_iterable(
+	[
+		make_webpy_controllers(status, class_name) 
+		for class_name, status 
+		in tests.items()
+	]
+))
+
+# include any manual test controllers by searching for all subclasses of TestController 
+# (but not TestController itself) in the local scope
+controllers += [
+	obj
+	for object_name, obj
+	in locals().items() 
+	if type(obj) == type and issubclass(obj, TestController) and obj != TestController
+]
+
+# sort test controllers by response code and class name for testing in order
+controllers = sorted(controllers, key=lambda cls: (cls.status, cls.__name__))
+
+#### Nose testing functions
 
 def setup():
+	'''Set up a web.py application to use for testing.'''
 	global test_app
+	# generate a tuple of paths and controller classes to set up web.py routing
+	urls = tuple(itertools.chain.from_iterable(
+		[
+			(controller.path(), controller) 
+			for controller 
+			in controllers
+		]
+	))
+	app = web.application(urls, globals())
 	test_app = TestApp(app.wsgifunc())
 
-def test_runner():
-	for cls in classes:
-		yield run, cls.path(), cls.status, cls.body
+def test():
+	'''Test runner for coverage of all classes.'''
+	print "%d classes to test" % len(list(controllers))
+	for cls in controllers:
+		yield run, cls
 
-def run(path, status, body):
-	res = test_app.get(path, expect_errors=True)
-	assert_equal(res.status, status)
-	assert_equal(res.body, body)
+def run(cls):
+	'''Run a specific test using the web.py test API to call and check a particular path.'''
+	response = test_app.get(cls.path(), expect_errors=True)
+	assert_equal(response.status, cls.status)
+	if hasattr(cls.body, 'keys'):
+		# we expect a dictionary, so json decode the body and compare raw dicts
+		try: 
+			assert_equal(json.loads(response.body), cls.body)
+		except ValueError,e:
+			print "Error decoding JSON body (%s):\n\t%s\nexpected:\n\t%s" % (e, response.body, json.dumps(cls.body))
+			raise
+	else:
+		assert_equal(response.body, cls.body)
+
+if __name__ == '__main__':
+	import sys
+	sys.stderr.write("Error, please run tests using nosetests.\n")
+	sys.exit(1)
 
--- webpy_helpers.py
+++ webpy_helpers.py
@@ -6,33 +6,107 @@ try:
 except ImportError:
 	import json
 
-class BadRequest(web.HTTPError):
-	'''Allow customized messages on 400 errors.'''
-	def __init__(self,message="bad request",headers={"Content-type":"text/html"}):
-		status = "400 Bad Request"
-		web.HTTPError.__init__(self, status, headers, message)
-class JsonBadRequest(BadRequest):
-	'''Allow customized JSON responses on 400 errors.'''
-	def __init__(self,
-					message={"message":"Bad request"},
-					headers={"Content-type":"application/json"}
-				):
-		if isinstance(message, basestring):
-			message = {"message":unicode(message)}
-		BadRequest.__init__(self, json.dumps(message), headers)
+#### Util class and function 
 
-class NotFound(web.HTTPError):
-	'''Allow customized messages on 404 errors.'''
-	def __init__(self,message="Not found",headers={"Content-type":"text/html"}):
-		status = "404 Not Found"
-		web.HTTPError.__init__(self, status, headers, message)
-class JsonNotFound(NotFound):
-	'''Allow customized JSON responses on 404 errors.'''
-	def __init__(self,
-					message={"message":"Not found"},
-					headers={"Content-type":"application/json"}
+class _HTTPError(web.HTTPError):
+	"""Base class for customized error message bodies"""
+	message = None
+	def __init__(self,message=None,headers={"Content-type":"text/html"}):
+		if message == None:
+			message = self.message if self.message else self.status[1]
+		web.HTTPError.__init__(self, " ".join(self.status), headers, message)
+		#print(self, " ".join(self.status), headers, message if message else self.status[1])
+
+def _make_json_init(init_f):
+	"""Wrap an error class __init__ function and prep to return JSON, not HTML."""
+	def __init__(
+				self,
+				message=None,
+				headers={"Content-type":"application/json"}
 				):
+		if not message:
+			message = {"message":self.status[1]}
 		if isinstance(message, basestring):
 			message = {"message":unicode(message)}
-		NotFound.__init__(self, json.dumps(message), headers)
+		init_f(self, json.dumps(message), headers)
+	return __init__
+
+class _JsonHttpErrorMeta(type):
+	'''Metaclass to create error subclass with JSON response body from _HTTPError subclass.'''
+	def __new__(self, name, bases, dir):
+		overrides = {
+			'__init__': _make_json_init(bases[0].__init__),
+			'__doc__': "%s with JSON response body" % bases[0].__doc__,
+		}
+		# override __init__ and __doc__ of base class, but only if not explicitly set in subclass
+		overrides.update(dir)
+		return type.__new__(self, name, bases, overrides)
+
+#### API
+class BadRequest(_HTTPError):
+	'''Allow customized messages on 400 errors'''
+	status = "400", "Bad Request"
+class JsonBadRequest(BadRequest):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class Unauthorized(_HTTPError):
+	'''Allow customized messages on 401 errors'''
+	status = "401", "Unauthorized"
+class JsonUnauthorized(Unauthorized):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class Forbidden(_HTTPError):
+	'''Allow customized messages on 403 errors'''
+	status = "403", "Forbidden"
+class JsonForbidden(Forbidden):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class NotFound(_HTTPError):
+	'''Allow customized messages on 404 errors'''
+	status = "404", "Not Found"
+class JsonNotFound(NotFound):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class MethodNotAllowed(_HTTPError):
+	'''Allow customized messages on 405 errors'''
+	status = "405", "Method Not Allowed"
+class JsonMethodNotAllowed(MethodNotAllowed):
+	__metaclass__ = _JsonHttpErrorMeta
+# for web.py compat
+NoMethod = MethodNotAllowed
+JsonNoMethod = JsonMethodNotAllowed
+
+class Gone(_HTTPError):
+	'''Allow customized messages on 410 errors'''
+	status = "410", "Gone"
+class JsonGone(Gone):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class Conflict(_HTTPError):
+	'''Allow customized messages on 409 errors'''
+	status = "409", "Conflict"
+class JsonConflict(Conflict):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class UnsupportedMediaType(_HTTPError):
+	'''Allow customized messages on 415 errors'''
+	status = "415", "Unsupported Media Type"
+class JsonUnsupportedMediaType(UnsupportedMediaType):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class RequestedRangeNotSatisfiable(_HTTPError):
+	'''Allow customized messages on 415 errors'''
+	status = "416", "Requested Range Not Satisfiable"
+class JsonRequestedRangeNotSatisfiable(RequestedRangeNotSatisfiable):
+	__metaclass__ = _JsonHttpErrorMeta
+
+class InternalServerError(_HTTPError):
+	'''Allow customized messages on 500 errors'''
+	status = "500", "Internal Server Error"
+class JsonInternalServerError(InternalServerError):
+	__metaclass__ = _JsonHttpErrorMeta
+# web.py compat
+InternalError = InternalServerError
+JsonInternalError = JsonInternalServerError
+
 
