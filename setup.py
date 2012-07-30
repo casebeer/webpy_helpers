de33bc3d1597b6244d93fee073b46fd43f24b740
Christopher H. Casebeer <christopher@chc.name>
2012-07-30 04:23:45 -0400

    Update setup.py, remove distribute_setup.py usage.
    Add nosetests to setup.py via collector.

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-from distribute_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 
 required_modules = [
@@ -10,15 +7,29 @@ required_modules = [
 	"web.py",
 	]
 
+readme = """
+Web.py Helpers
+"""
+
 setup(
 	name="webpy_helpers",
 	version="0.0.2",
 	description="",
+
 	author="Christopher H. Casebeer",
 	author_email="",
-	url="",
-	py_modules=["webpy_helpers", "distribute_setup"],
-	#packages=find_packages(exclude='tests'),
-	install_requires=required_modules
-	)
+	url="https://github.com/casebeer/webpy_helpers",
+
+	py_modules=["webpy_helpers"],
+	install_requires=required_modules,
+
+	tests_require=["nose"],
+	test_suite="nose.collector",
+
+	long_description=readme,
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Intended Audience :: Developers",
+	]
+)
 
